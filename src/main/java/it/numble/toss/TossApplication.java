9f05c71687610f96f4ac054394b68107e62d4cd1
kook1932 <gkswjddyd4@naver.com>
2023-06-26 22:17:22 +0900

    Initial commit

---
+++ src/main/java/it/numble/toss/TossApplication.java
@@ -0,0 +1,13 @@
+package it.numble.toss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/numble/toss/TossApplicationTests.java
@@ -0,0 +1,13 @@
+package it.numble.toss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
